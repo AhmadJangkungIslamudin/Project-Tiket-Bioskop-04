89aac1bdf847541a2027e3c3cf43c07dfb936364
AhmadJangkungIslamudin <ahmadjangkungi@student.uns.ac.id>
2023-06-17 23:32:24 +0700

    final program

--- emailsender.py
+++ emailsender.py
@@ -11,31 +11,18 @@ SMTP_USERNAME = 'bioskopadudu@gmail.com' #Email Pengirim
 SMTP_PASSWORD = 'bqeseknufgjpsxkr' #Password SMTP EMAIL
 SENDER_EMAIL = 'bioskopadudu@gmail.com' # Email Pengirim
 
-def validate_email(email):
-    # Pola regular expression untuk validasi alamat email
-    pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-    return re.match(pattern, email)
-
-# Meminta input dari pengguna untuk alamat email penerima
-while True:
-    RECIPIENT_EMAIL = input("Masukkan Email Anda: ")
-    if validate_email(RECIPIENT_EMAIL):
-        break
-    else:
-        print("Alamat email tidak valid. Silakan coba lagi.")
-
-
 # Membuat pesan email
 msg = MIMEMultipart()
 msg['From'] = SENDER_EMAIL
 msg['To'] = RECIPIENT_EMAIL
-msg['Subject'] = 'REKAP PEMESANAN'
+msg['Subject'] = 'TIKET ADUDU CINEMAX'
 
-body = 'Halo, ini adalah contoh email 2.'
+body = "wrwvv"
 msg.attach(MIMEText(body, 'plain'))
 simple_email_contatcs = ssl.create_default_context()
 
 # Mengirim email menggunakan SMTP
+
 try:
     print("Connecting to server ... ")
     server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
